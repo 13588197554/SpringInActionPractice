157edaef523ae2425467f3d2f28438af1e07b7fb
MilesG666 <gxx632364@gmail.com>
2018-02-24 21:43:50 +0800

    Spring之旅第八站：Spring MVC Spittr舞台的搭建、基本的控制器（JavaConfig）、请求的输入、表单验证、测试(重点)

--- Chapter_05/Spittr/src/main/java/com/guo/spittr/data/SpittleRepository.java
+++ Chapter_05/Spittr/src/main/java/com/guo/spittr/data/SpittleRepository.java
@@ -8,5 +8,12 @@ import java.util.List;
  * Created by guo on 24/2/2018.
  */
 public interface SpittleRepository {
+
+    List<Spittle> findRecentSpittles();
+
     List<Spittle> findSpittles(long max, int count);
+
+    Spittle findOne(long id);
+
+    void save(Spittle spittle);
 }
